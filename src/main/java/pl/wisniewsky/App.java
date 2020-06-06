bdeba5d30a03aad00f2c139d492d5752d3846066
Wisniewski98 <bwisniewski095@gmail.com>
2020-06-06 12:11:40 +0200

    first commit

---
+++ src/main/java/pl/wisniewsky/App.java
@@ -0,0 +1,13 @@
+package pl.wisniewsky;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Witam elo elo wszystko działa" );
+    }
+}
